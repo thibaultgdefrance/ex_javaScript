d7a43c14ded293390e609b6bebb868f82f4344cc
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-02-06 11:10:12 +0100

    -debut_javaScript

---
+++ ex_javaScript.js
@@ -0,0 +1,6 @@
+function nom(){
+    document.getElementById("nom").textContent=document.getElementById("saisie").value;
+}
+function couleur(){
+    document.body.style.backgroundColor=document.getElementById("saisie2").value;
+}
