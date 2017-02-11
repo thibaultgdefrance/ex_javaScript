273622c2b373268dda20e02a7213014eff6e25b0
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-02-11 22:21:13 +0100

    -

--- jeu_du_pendu.js
+++ jeu_du_pendu.js
@@ -2,8 +2,8 @@ var mots= ["jeu","clafoutis","pendrie","magenta","verbale","savoureux","entier",
 
 //var secret= mots[Math.floor(Math.random()*mots.lengt)];
 
-function alphabet(lettre) {
-        var lettre=
-        echo(lettre) ;
-        console.log(lettre) ;
+function alphabet() {
+
+        return(document.getElementById("alphabet").value) ;
+
 }
