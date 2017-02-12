2736704d04ad436c407c7963a2a2a4c4bcf12882
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-02-12 22:29:16 +0100

    -brouillon

--- jeu_du_pendu.js
+++ jeu_du_pendu.js
@@ -1,9 +1,22 @@
 var mots= ["jeu","clafoutis","pendrie","magenta","verbale","savoureux","entier",];
 
 var secret = mots[Math.floor(Math.random()*mots.length)];
-
-alert(secret);
 var lettre=getElementsByClassName('alphabet').value=value;
-function choix(lettre) {
-    console.log(lettre);
+
+//function choix(lettre) {
+//    console.log(lettre);
+//}
+
+function jeu(){
+    var erreur = 0;
+    var i = 0;
+    foreach(i<secret.length-1,erreur<11, i++){
+        if lettre===secret[i]{
+            console.log("ok");
+        }
+        else {
+            erreur++;
+        }
+    }
 }
+jeu;
