c7e0c07058d5feea42b8391f2f89eb4c962f04e2
Thibault Defrance <thibault.g.defrance@gmail.com>
2017-02-12 22:16:18 +0100

    -ajout_var_random

--- jeu_du_pendu.js
+++ jeu_du_pendu.js
@@ -1,6 +1,8 @@
 var mots= ["jeu","clafoutis","pendrie","magenta","verbale","savoureux","entier",];
 
-//var secret= mots[Math.floor(Math.random()*mots.lengt)];
+var secret = mots[Math.floor(Math.random()*mots.length)];
+
+alert(secret);
 var lettre=getElementsByClassName('alphabet').value=value;
 function choix(lettre) {
     console.log(lettre);
